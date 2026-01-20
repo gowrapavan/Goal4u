ed11924c0b767660b6af7d8637fc44711b0f5810
gowrapavan <gowrapavankumar2004@gmail.com>
2026-01-21 03:26:52 +0530

    live

--- src/components/HomeTV.jsx
+++ src/components/HomeTV.jsx
@@ -10,8 +10,6 @@ const PROVIDER_CODES = {
   yallashooote: "yallashooote",
   livekora: "vip",
   shahidkoora: "shk",
-  sirtv: "sirtv",
-  soccerhd: "socchd"
 };
 const CODE_TO_PROVIDER = Object.fromEntries(
   Object.entries(PROVIDER_CODES).map(([k, v]) => [v, k])
@@ -25,8 +23,6 @@ const PROVIDERS = [
   { label: "yallashooote", keyword: "yallashooote" },
   { label: "livekora", keyword: "livekora" },
   { label: "Shahid-Koora", keyword: "shahidkoora" },
-  { label: "sirtv", keyword: "sirtv" },
-  { label: "soccerhd", keyword: "soccerhd" }
 ];
 
 // --- JSON Sources ---
@@ -36,9 +32,7 @@ const JSON_MAP = {
   livekora: "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/json/livekora.json",
   shahidkoora: "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/json/shahidkoora.json",
   hesgoal: "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/json/hesgoal.json",
-  yallashooote: "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/json/yallashooote.json",
-  sirtv: "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/json/siiir.json", //new
-  soccerhd: "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/json/soccerhd.json"//new
+  yallashooote: "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/json/yallashooote.json"
 };
 
 // --- Helpers ---
